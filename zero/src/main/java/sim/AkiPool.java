e90565a27a3620ffc684207f0321e6fadd1f2f57
wjkanaya <wjkanaya@gmail.com>
2020-09-17 07:51:23 +0900

    a

--- zero/src/main/java/sim/AkiPool.java
+++ zero/src/main/java/sim/AkiPool.java
@@ -1,8 +1,34 @@
 package sim;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import deus_proto.Member;
+
 public class AkiPool {
 
-	
-	
-	
+	public List<Set<Member>> akiList = new LinkedList<Set<Member>>();
+
+
+	public void setAkiMember(Member mem, int itukara) {
+
+		while (akiList.size() < itukara + 1) {
+			akiList.add(new HashSet<Member>());
+		}
+		akiList.get(itukara).add(mem);
+	}
+
+	public void inc() {
+
+		if (akiList.size() > 1) {
+			Set<Member> oneSet = akiList.get(1);
+			akiList.get(0).addAll(oneSet);
+			akiList.remove(1);
+		}
+	}
+
+
+
 }

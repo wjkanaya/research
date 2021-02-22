61797ab1f95daf2885bc59b23ca3f95726ddd152
wjkan <wjkan@192.168.0.6>
2021-02-22 14:28:04 +0900

    a

---
+++ zero/src/main/java/deus_proto/Estimate.java
@@ -0,0 +1,22 @@
+package deus_proto;
+
+import java.util.List;
+
+import org.apache.commons.math3.linear.RealMatrix;
+
+public interface Estimate {
+
+	public void clear();
+	public void getData(List<Integer> searchYearsList,List<String> targetCodeList);
+	public void makeData();
+	// public void clearCache();
+	public void setCovariatesValue(RealMatrix lastBetaMat);
+	public void setCovariatesValueBeta0(RealMatrix lastBetaMat);
+
+
+	public int getBetaSize();
+	public void setBetaSize(int betaSize);
+	public double L(double[][] beta);
+	public RealMatrix delta(RealMatrix betaMat);
+
+}
